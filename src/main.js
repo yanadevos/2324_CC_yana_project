a634d34fa616c0987338107db436c640830710f6
Yana De Vos <yana.devos@student.ap.be>
2024-03-04 11:01:54 +0100

    essentia toegevoegd

--- src/main.js
+++ src/main.js
@@ -1,2 +1,15 @@
-import './styles/style.css'
+import Essentia from "https://cdn.jsdelivr.net/npm/essentia.js@0.1.3/dist/essentia.js-core.es.js";
+// import essentia-wasm-module
+import { EssentiaWASM } from "https://cdn.jsdelivr.net/npm/essentia.js@0.1.3/dist/essentia-wasm.es.js";
 
+const essentia = new Essentia(EssentiaWASM);
+
+/*
+// run an specific algorithm
+let yourOutput = essentia.'<your-essentia-algo>'(<inputs> ..., <parameters> (optional)...);
+
+yourOutput.'<your_output-key>'/*/
+
+console.log(essentia.version);
+// prints all the available algorithms in essentia.js
+console.log(essentia.algorithmNames);
